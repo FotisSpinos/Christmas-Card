84543e5550e1c3db4e3b0191107788a67fcb9d9d
Live session user <fotis.spinos23@gmail.com>
2020-12-22 20:43:19 +0000

    Added aspect ratio functionality

--- Source/configurations.js
+++ Source/configurations.js
@@ -1,9 +1,10 @@
 settings = {
     RefeashRate: 33,
+    aspectRatio: 1/1
 }
 
 assets = {
     snowflakeSrc: "../Assets/Snowflake.png",
     backgroundSrc: "../Assets/background.webp",
-    audioSrc: "../Assets/Jingle Bells with Lyrics Christmas Songs HD Christmas Songs and Carols.mp3"
+    audioSrc: "../Assets/Jingle Bells with Lyrics Christmas Songs HD Christmas Songs and Carols.mp3",
 }
